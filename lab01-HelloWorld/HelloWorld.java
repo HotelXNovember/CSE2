b8830f1e2e889476cd3364d8320c30f9292fb1a6
lorenrailsback <loren.railsback@gmail.com>
2015-08-27 15:15:01 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//Loren Railsback
+//Hello World Java Program
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+        
+}
