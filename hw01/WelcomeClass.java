1f913c67d94b21fb51e21acd1746e42753aee8e7
lorenrailsback <loren.railsback@gmail.com>
2015-08-28 20:30:28 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Loren Railsback
+//Welcome Class Java Program
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the statement with box around it
+        System.out.println("       -----------");
+        System.out.println("       | Welcome |");
+        System.out.println("       -----------");
+        System.out.println("     ^  ^  ^  ^  ^  ^");
+        System.out.println("    / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("   |-L--M--R--2--1--9-|");
+        System.out.println("    \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("     v  v  v  v  v  v");
+        System.out.println("I was born in Texas but grew up in Indiana. I have been to 13 different countries. Now I go to Lehigh University.");
+    }
+        
+}
